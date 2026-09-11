d47bc3e28bb20494b1842716f5a7ff3bc59631c3
ajay-kumar <ajay.kumar@clover.com>
2026-09-11 14:40:23 +0100

    chore: initial commit :rocket:

--- minikube.sh
+++ minikube.sh
@@ -42,48 +42,7 @@ kubectl delete service kube-dns -n kube-system --ignore-not-found
 kubectl delete serviceaccount coredns -n kube-system --ignore-not-found
 kubectl delete configmap coredns -n kube-system --ignore-not-found
 
-echo "=== Step 5: Creating argo-system Namespace & Injecting Zscaler CA ConfigMap ==="
-kubectl create namespace argo-system --dry-run=client -o yaml | kubectl apply -f -
-kubectl create configmap argocd-tls-certs-cm \
-  -n argo-system \
-  --from-file=github.com="${ZSCALER_CERT}" \
-  --from-file=ghcr.io="${ZSCALER_CERT}"
-
-echo "=== Step 6: Bootstrapping Applications (Namespaces, Cilium, CoreDNS, ArgoCD) ==="
+echo "=== Step 5: Bootstrapping Applications (Namespaces, Cilium, CoreDNS, ArgoCD) ==="
 just bootstrap apps
-
-echo "=== Step 7: Applying Cilium Network Policy for ArgoCD Repo Server ==="
-kubectl patch deployment argocd-repo-server -n argo-system --type json -p '[
-  {
-    "op": "add",
-    "path": "/spec/template/spec/volumes/-",
-    "value": {
-      "name": "zscaler",
-      "configMap": {
-        "name": "argocd-tls-certs-cm"
-      }
-    }
-  },
-  {
-    "op": "add",
-    "path": "/spec/template/spec/containers/0/volumeMounts/-",
-    "value": {
-      "name": "zscaler",
-      "mountPath": "/etc/ssl/certs/zscaler.crt",
-      "subPath": "zscaler.crt"
-    }
-  },
-  {
-    "op": "add",
-    "path": "/spec/template/spec/containers/0/env/-",
-    "value": {
-      "name": "SSL_CERT_FILE",
-      "value": "/etc/ssl/certs/zscaler.crt"
-    }
-  }
-]'
-
-echo "=== Step 8: Restarting ArgoCD Repo Server ==="
-kubectl rollout restart deployment argocd-repo-server -n argo-system
 
 echo "=== Setup complete! ==="
